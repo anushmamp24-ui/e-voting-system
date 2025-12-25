c78dd176c402403bddb3d58fcd92c5e03a255532
anushma <anushmamp24@gmail.com>
2025-12-25 14:31:12 +0530

    changed registration module. added password protection feature

--- backend/api/userregistration.php
+++ backend/api/userregistration.php
@@ -3,9 +3,20 @@ include("../config/db.php");
 
 $data = json_decode(file_get_contents("php://input"), true);
 
+if (!$data) {
+    echo json_encode(["error" => "No input data received"]);
+    exit;
+}
+
+
 $name = $data['name'];
 $email = $data['email'];
 $password = password_hash($data['password'], PASSWORD_DEFAULT);
+
+if (empty($name) || empty($email) || empty($password)) {
+    echo json_encode(["error" => "All fields are required"]);
+    exit;
+}
 
 $sql = "INSERT INTO users(name,email,password,role) 
         VALUES ('$name','$email','$password','VOTER')";
